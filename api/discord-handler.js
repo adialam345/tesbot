bb0c7a0a9e6042a5dc383d6113135dad8448ec77
adialam345 <156929465+adialam345@users.noreply.github.com>
2024-02-10 22:23:00 +0700

    Update discord-handler.js

--- api/discord-handler.js
+++ api/discord-handler.js
@@ -1,14 +1,30 @@
-// api/discord-handler.js
-module.exports = async function (req, res) {
+const axios = require('axios');
+
+module.exports = async (req, res) => {
   try {
-    const { messages } = req.body;
+    const { message } = req.body;
+    const channelId = '1205841803623006208'; // Ganti dengan ID saluran Discord Anda
+    const webhookUrl = 'https://discordapp.com/api/webhooks/1205867563490222150/SEVhk7dD0JpcOoQH32ea4nvA3_5181TMyH0eSwku7NP6MD_9DFRTqbLd3ywuOJbh7PDe'; // Ganti dengan URL Webhook Discord Anda
 
-    // Lakukan apa yang Anda butuhkan dengan data pesan di sini
-    console.log('Received messages:', messages);
+    const formattedMessage = `
+      **NAMA:** ${message.author ? `${message.author.givenName} ${message.author.familyName}` : 'Unknown'}
+      **Message:** ${formatMessage(message.message)}
+      **WAKTU:** ${new Date(message.createdAt).toLocaleString('en-US', { timeZone: 'Asia/Jakarta' })}
+    `;
 
-    res.status(200).json({ success: true, message: 'Messages received successfully.' });
+    // Kirim pesan ke Discord menggunakan Webhook
+    await axios.post(webhookUrl, {
+      content: formattedMessage,
+    });
+
+    res.status(200).json({ success: true, message: 'Pesan dikirim ke Discord!' });
   } catch (error) {
-    console.error('Error handling Discord messages:', error);
+    console.error('Error:', error);
     res.status(500).json({ success: false, error: 'Internal Server Error' });
   }
+};
+
+const formatMessage = (text) => {
+  const urlRegex = /(https?:\/\/[^\s]+)/g;
+  return text.replace(urlRegex, url => `[${url}](${url})`);
 };
