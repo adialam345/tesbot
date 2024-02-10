3e3f26f92d430b7b15d9fab62cc0426d150ddcb7
adialam345 <156929465+adialam345@users.noreply.github.com>
2024-02-10 20:52:36 +0700

    Update discord-handler.js

--- discord-handler.js
+++ discord-handler.js
@@ -1,26 +1,50 @@
-const fetch = require('node-fetch');
+const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-module.exports = async (req, res) => {
+const app = express();
+app.use(bodyParser.json());
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+app.post('/discord-handler', (req, res) => {
   try {
-    const { message } = req.body;
-    const webhookUrl = 'https://discordapp.com/api/webhooks/1205867563490222150/SEVhk7dD0JpcOoQH32ea4nvA3_5181TMyH0eSwku7NP6MD_9DFRTqbLd3ywuOJbh7PDe'; // Ganti dengan URL webhook Discord Anda
-    
-    const response = await fetch(webhookUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ content: message }),
+    const { messages } = req.body;
+
+    const channel = client.channels.cache.get('1205841803623006208'); // Replace with your channel ID
+
+    messages.forEach((message) => {
+      const formattedMessage = `
+        **NAMA:** ${message.author.givenName} ${message.author.familyName || ''}
+        **Message:** ${message.message}
+        **WAKTU:** ${new Date(message.createdAt).toLocaleString('en-US', { timeZone: 'Asia/Jakarta' })}
+      `;
+
+      channel.send(formattedMessage);
     });
 
-    if (response.ok) {
-      res.status(200).json({ success: true, message: 'Pesan dikirim ke Discord!' });
-    } else {
-      const errorText = await response.text();
-      res.status(response.status).json({ success: false, error: errorText });
-    }
+    res.status(200).send('Messages sent to Discord channel successfully.');
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ success: false, error: 'Internal Server Error' });
+    console.error('Error handling Discord messages:', error);
+    res.status(500).send('Internal Server Error');
   }
-};
+});
+
+// Connect to Discord
+client.login('MTE2ODE2NDMzNzA3MzAxNjkwMg.G4ToOu.YEyUz3lqQPPw2CVOT_cAikf_wW79A1bNpEWeuw');
+
+// Start Express server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
